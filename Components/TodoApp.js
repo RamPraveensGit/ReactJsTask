63ff107c0a6d936fe1a5dd2c5eeb3fe39c19a5cf
RamPraveen R <rampraveen.r@ind.xebiadigital.com>
2024-05-30 18:56:09 +0530

    todoList

--- Components/TodoApp.js
+++ Components/TodoApp.js
@@ -1,61 +1,58 @@
-    import React, { useState , useCallback, useMemo } from 'react';
-    import '../App.css';
+import React, { useState, useCallback, useMemo } from 'react';
+import '../App.css';
 
-    const TodoApp = () => {
-    const [todo, setTodo]= useState([]);
-    const [input , setInput] = useState(0);
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
 
-    const addTodo = useCallback(()=>{
-        if (input !== "") {
-            const newTodo = [...todo];
-            newTodo.push(input);
-        setTodo(newTodo);
-        setInput("");
-        }
-    },[input]);
-    const delTodo = useCallback ((inn)=>{
-        function dt(i) {
-            return i !== inn
+  const addTodo = useCallback(() => {
+    if (input.trim() !== "") {
+      setTodos(prevTodos => [...prevTodos, input]);
+      setInput('');
     }
-        const dtodo = todo.filter(dt);
-        setTodo(dtodo);        
-    },[])
+  }, [input]);
 
-    const handleEditTodo = (index, newValue) => {
-        const updatedTodos = [...todo];
-                updatedTodos[index] = newValue;
-                setTodo(updatedTodos);
-      };
-      const tt = useMemo(() => (
-        todo.map((todo, index) => (
-          <div key={index}>
-            <span>{todo}</span>
-            <button onClick={() => delTodo(index)}>Delete</button>
-            <button onClick={() => {
-              const newTodo = prompt('Edit todo:', todo);
-              if (newTodo !== null) {
-                handleEditTodo(index, newTodo);
-              }
-            }}>Edit</button>
-          </div>
-        ))
-      ), [todo, delTodo, handleEditTodo]);
+  const delTodo = useCallback((index) => {
+    setTodos(prevTodos => prevTodos.filter((_, i) => i !== index));
+  }, []);
 
-    return (
-        <div>
-        <h1>To-Do List</h1>
-        <input
+  const handleEditTodo = (index, newValue) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = newValue;
+    setTodos(updatedTodos);
+  };
+
+  const todoList = useMemo(() => (
+    todos.map((todo, index) => (
+      <div className="todo-item" key={index}>
+        <span>{todo}</span>
+        <button className="todo-button delete" onClick={() => delTodo(index)}>Delete</button>
+        <button className="todo-button edit" onClick={() => {
+          const newTodo = prompt('Edit todo:', todo);
+          if (newTodo !== null) {
+            handleEditTodo(index, newTodo);
+          }
+        }}>Edit</button>
+      </div>
+    ))
+  ), [todos, delTodo]);
+
+  return (
+    <div className="todo-container">
+      <h1>To-Do List</h1>
+      <input
+        className="todo-input"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Enter todo..."
-        />
-        <button onClick={addTodo}>Add Todo</button>
-        <div>
-        {tt}
-        </div>
+      />
+      <button className="todo-button add" onClick={addTodo}>Add Todo</button>
+      <div>
+        {todoList}
+      </div>
     </div>
-    );
-    };
+  );
+};
 
-    export default TodoApp;
+export default TodoApp;
